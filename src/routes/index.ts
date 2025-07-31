cdf02e2cb65ddc661ee8e5eb5ac1ece876423438
Kiernan Soriano <nukabunny@gmail.com>
2025-07-31 16:52:26 -0700

    Added all needed routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,21 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const router = Router();
 
-router.get("/", (req, res) => res.render("index", {}));
+/**
+ * ------------------ GET ROUTES ------------------------
+ */
+
+router.get('/', (req, res) => res.render('index', {}));
+
+router.get('/sign-up', (req, res) => res.render('sign-up', {}));
+
+router.get('/join', (req, res) => res.render('join', {}));
+
+router.get('/login', (req, res) => res.render('login', {}));
+
+/**
+ * ------------------ POST ROUTES ------------------------
+ */
 
 export default router;
